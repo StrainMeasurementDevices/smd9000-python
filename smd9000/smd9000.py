cd24928ad6598ecbf77ce93fa2c9782ef9985a7d
Electro707 <general@electro707.com>
2022-02-24 17:17:01 -0500

    Function cleanups and new functions.
    Added the `get_meter_constant` function. Fixed other functions

--- smd9000/smd9000.py
+++ smd9000/smd9000.py
@@ -2,7 +2,10 @@
 """
     SMD9000 Python Package
 """
+import struct
 import sys
+import typing
+
 import serial                                   # Import pyserial to interface to the sensor
 import serial.tools.list_ports as serial_tools  # Import the list_ports tools from pyserial to find the SMD9000 sensor.
 import logging                                  # Import logging to log
@@ -29,6 +32,9 @@ class SMD9000:
         self._read_thread = threading.Thread(target=self._read_thread)
 
     def get_available_sensors(self) -> list:
+        """
+        Returns: A list of available SMD9000 sensors
+        """
         available_list = []
         device_list = serial_tools.comports()
         for x in device_list:
@@ -47,9 +53,22 @@ class SMD9000:
         return available_list
 
     def connect(self, device: str = None) -> bool:
+        """
+        Connects to a SMD9000 sensor.
+
+        Args:
+            device (str, Optional): An optional device name (`COM3` for Win32 or `/dev/ttyUSB0` for UNIX for example) to connect to. If omitted, this function will try to automatically detect one with the :func:`get_available_sensors` function.
+
+        Returns:
+            True or False depending on whether the device connected or now. True meaning it found a device and successfully connected to it
+
+        Raises:
+            UserWarning: If this class is ready connected to an SMD9000 sensor
+
+        """
         if self.is_connected is True:
-            self._log.warning('Already connected to SMD9000')
-            return False
+            self._log.warning('Already connected to an SMD9000 sensor')
+            raise UserWarning('Already connected to an SMD9000 sensor')
         device_connection_list = []
         if device is None:
             device_connection_list = self.get_available_sensors()
@@ -64,7 +83,6 @@ class SMD9000:
                 if self._ser is not None:
                     self._ser.close()
                 continue
-
             self._log.info('Found SMD9000 on port %s' % d)
             self.is_connected = True
             self._ser.flush()
@@ -76,17 +94,36 @@ class SMD9000:
         This is a thread that continuously reads from the SMD9000 sensor. Used when streaming is enabled
         """
         pass
-      
-    def get_adc_capture(self):
+
+    def get_meter_constant(self) -> float:
         """
-        Gets a single ADC capture waveform for upstream and downstream measurments
+        Get the meter constant currently in use by the sensor
+
+        Returns: The meter constant
+        """
+        self._ser_write('getmc')
+        mc = self._ser_read_until()
+        mc = self.ieee_float_to_python(mc)
+        return mc
+
+    def get_adc_capture(self) -> typing.Tuple[list, list]:
+        """
+        Gets a single ADC capture waveform for upstream and downstream measurements
+
+        Returns: A tuple contaning two list, the first for the downstream ADC data and the second for the upstream ADC data.
         """
         self._ser_write('adccapture')
         ups = self._ser_read_until()
         dns = self._ser_read_until()
-        
-        ups = ups.split(',')
-        dns = dns.split(',')
+        # Check for the starting string
+        if not ups.startswith('ups'):
+            raise SMD9000ReadException("Read data did not include the 'usp' header")
+        if not dns.startswith('dnd'):
+            raise SMD9000ReadException("Read data did not include the 'dns' header")
+        # Split the data by commas and remove the header
+        ups = ups.split(',')[1:]
+        dns = dns.split(',')[1:]
+        return ups, dns
 
     def check_if_device_is_smd9000(self):
         """
@@ -103,22 +140,26 @@ class SMD9000:
     def get_info(self) -> SMD9000Info:
         """
         Gets the hardware and firmware revision from the sensor
+
+        Returns:
+            An SMD9000Info dataclass containing the hardware revision, and the firmware major and minor revision
+
+        Raises:
+            UserWarning: If the returned firmware numbers are not able to be turned into an integer
+            SMD9000ReadException: If an error occured while reading the sensor's versions
         """
         self._ser_write('getversion')
-        dev_name = self._ser_read_until()             # Read the hardware and firmware revision
-        dev_name = dev_name.split(',')                # Split the return string by comma
-        dev_name = [d.split(":") for d in dev_name]   # Split the comma seperated string by colon
-        dev_name = [d.strip() for d in dev_name]      # Remove whitespacing
-        if dev_name[0] != "Hardware:":
-            raise UserWarning()
-        if dev_name[2] != "Firmware:":
-            raise UserWarning()
-        ret = SMD9000Info()
-        ret.hardware_rev = dev_name[1]
-        f = dev_name[3].split(".")
+        dev_name = self._ser_read_until()                   # Read the hardware and firmware revision
+        hardware_rev, firmware_rev = dev_name.split(',')    # Split the return string by comma
+        hardware_rev = hardware_rev.split(":")
+        firmware_rev = firmware_rev.split(":")
+        if hardware_rev[0] != "Hardware:":
+            raise SMD9000ReadException()
+        if firmware_rev[0] != "Firmware:":
+            raise SMD9000ReadException()
+        f = firmware_rev[1].split(".")
         try:
-            ret.firmware_major_rev = int(f[0])
-            ret.firmware_minor_rev = int(f[1])
+            ret = SMD9000Info(hardware_rev=hardware_rev[1], firmware_major_rev=int(f[0]), firmware_minor_rev=int(f[1]))
         except ValueError:
             raise UserWarning("Invalid return: Unable to integerize the firmware revision")
         return ret
@@ -153,3 +194,11 @@ class SMD9000:
                 return ret
             except serial.SerialException:
                 raise SMD9000ReadException()
+
+    @staticmethod
+    def ieee_float_to_python(ieee_float: str) -> float:
+        """
+        Takes a string in the form of an ASCII IEEE754 float, for example BE264F7B, and converts it to a floating
+        point number that Python can use
+        """
+        return struct.unpack("f", struct.pack("I", int(ieee_float, 16)))[0]
